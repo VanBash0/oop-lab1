a4b46c7188a4b3f83c1ee3ac838697adb49e6afd
Ivan Bashkov <bashkovia@mail.ru>
2026-09-23 11:51:15 +0300

    Created SKU

---
+++ oop-lab1/SKU.cs
@@ -0,0 +1,16 @@
+﻿namespace oop_lab1;
+
+public class Sku
+{
+    public int Id { get; }
+    public string Name { get; }
+    public double VolumeWeightChars { get; }
+
+    public Sku(int id, string name, double volumeWeightChars)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(volumeWeightChars);
+        Id = id;
+        Name = name;
+        VolumeWeightChars = volumeWeightChars;
+    }
+}
