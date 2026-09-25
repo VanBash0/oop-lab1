e541ec489c3678cd4c498f2b313e156e921efb3a
Ivan Bashkov <bashkovia@mail.ru>
2026-09-25 21:22:14 +0300

    Added route stage results

---
+++ oop-lab1/Results/RouteStageResults.cs
@@ -0,0 +1,21 @@
+﻿using oop_lab1.Logistics;
+using oop_lab1.SKU;
+
+namespace oop_lab1.Results;
+
+public record RouteStageResult
+{
+    private RouteStageResult() { }
+
+    public sealed record TruckInsufficientCapacity(Manifest RejectedManifest) : RouteStageResult;
+    
+    public sealed record TruckInsufficientStock(Manifest RejectedManifest) : RouteStageResult;
+    
+    public sealed record WarehouseInsufficientCapacity(Manifest RejectedManifest) : RouteStageResult;
+    
+    public sealed record WarehouseInsufficientStock(Manifest RejectedManifest) : RouteStageResult;
+    
+    public sealed record TruckTooFar(Warehouse Warehouse) : RouteStageResult;
+
+    public sealed record StageSuccess(TimeSpan TotalTime) : RouteStageResult;
+}
